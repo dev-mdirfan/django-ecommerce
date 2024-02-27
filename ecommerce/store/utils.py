54196560ecd16395f633ad5c064d21ab1414de15
Mohd Irfan <mdirfan5702@gmail.com>
2024-02-27 21:37:42 +0530

    utils update: items with negative quantity = lot of freebies

--- ecommerce/store/utils.py
+++ ecommerce/store/utils.py
@@ -3,40 +3,43 @@ from .models import *
 
 
 def cookieCart(request):
+    # Create empty cart for now for non-logged in user
     try:
         cart = json.loads(request.COOKIES['cart'])
     except:
         cart = {}
 
-    print('Cart:', cart)
+    # print('Cart:', cart)
     items = []
     order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
     cartItems = order['get_cart_items']
 
     for i in cart:
+        # We use try block to prevent items in cart that may have been removed from causing error
         try:
-            cartItems += cart[i]['quantity']
+            if(cart[i]['quantity']>0): #items with negative quantity = lot of freebies
+                cartItems += cart[i]['quantity']
 
-            product = Product.objects.get(id=i)
-            total = (product.price * cart[i]['quantity'])
+                product = Product.objects.get(id=i)
+                total = (product.price * cart[i]['quantity'])
 
-            order['get_cart_total'] += total
-            order['get_cart_items'] += cart[i]['quantity']
+                order['get_cart_total'] += total
+                order['get_cart_items'] += cart[i]['quantity']
 
-            item = {
-                'product': {
-                    'id': product.id,
-                    'name': product.name,
-                    'price': product.price,
-                    'imageURL': product.imageURL,
-                },
-                'quantity': cart[i]['quantity'],
-                'get_total': total
-            }
-            items.append(item)
+                item = {
+                    'product': {
+                        'id': product.id,
+                        'name': product.name,
+                        'price': product.price,
+                        'imageURL': product.imageURL,
+                    },
+                    'quantity': cart[i]['quantity'],
+                    'get_total': total
+                }
+                items.append(item)
 
-            if product.digital == False:
-                order['shipping'] = True
+                if product.digital == False:
+                    order['shipping'] = True
         except:
             pass
 
@@ -86,6 +89,8 @@ def guestOrder(request, data):
         orderItem = OrderItem.objects.create(
             product=product,
             order=order,
-            quantity=item['quantity']
+            # negative quantity = freebies
+            quantity=(item['quantity'] if item['quantity']
+                      > 0 else -1*item['quantity']),
         )
     return customer, order
